eb1655d718435926c8a0f033483c0b522d4a992f
limonn5 <92103116+limonn5@users.noreply.github.com>
2022-03-12 19:03:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+// // let b = 2
+// // let a = 1
+// // console.log(-b)
+// // console.log(b-a)
+
+
+
+// let c = 5
+// let d
+
+// d=5%2 //остаток от деления
+// console.log(d)
+
+// // let v = prompt('v?')
+// // let n = prompt('n?')
+// // console.log('v^n =', v**n)//поднесение в степень
+
+// let g = 'n'
+// let m ='o'
+// console.log(g+m)//no
+
+// x = 27
+// console.log(x**(1/3))
+
+
+// x = 27
+// console.log(x)
+// console.log(++x)
+// console.log(--x)
+
+
+// let a = +prompt('a =')
+// let b = +prompt('b =')
+// console.log('a =', a)
+// console.log('b =',b)
+// console.log(2*(a+b))
+// console.log(a*b)
+
+// let z = +prompt('z =')
+// console.log('z =', z)
+// console.log(z*4)
+// console.log(z*z)
+
+
+// let a = +prompt('a =')
+// let b = +prompt('b =')
+// console.log('a =', a)
+// console.log('b =',b)
+// let c = (a**2 +b**2)**1/2
+// console.log('c =', c)
+// console.log('периметр =', c+a+b)
+
+
+
+
+// let b = document.querySelector('.block1')
+
+
+let a = +prompt('a')
+function pow(){
+    let b = (a**2)
+    let v = document.querySelector('.block1')
+    v.innerHTML = `${b}`
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
